fef8c14fd5219e59d797992ebabcb9f0049bb226
Zar Ni Tun <zarnitunkt@gmail.com>
2024-02-22 22:51:12 +0630

    Add css,js files and initialize form

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -1,2 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template,request
 app = Flask(__name__)
+
+from stories import story
+
+@app.route("/")
+def madlib():
+    required = story.prompts
+    return render_template ("homepage.html",ask = required)
+
+# @app.route("/story")
+# def print_story():
+#     return render_template ("story.html")
