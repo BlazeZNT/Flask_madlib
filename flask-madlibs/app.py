aaa857d161920a739b64ac33b0e8d35cd6482603
Zar Ni Tun <zarnitunkt@gmail.com>
2024-02-23 08:07:03 +0630

    finish form and display and add css

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -8,6 +8,12 @@ def madlib():
     required = story.prompts
     return render_template ("homepage.html",ask = required)
 
-# @app.route("/story")
-# def print_story():
-#     return render_template ("story.html")
+@app.route("/story")
+def print_story():
+    new_dict = {}
+    for val in story.prompts:
+        new_dict[val] = request.args[val]
+    final_story = story.generate(new_dict)
+    return render_template("story.html",final_story = final_story)
+        
+
